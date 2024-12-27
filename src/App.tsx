8273ad71796c326c56d7743a7cef8b58a47b8c2f
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2024-12-26 23:28:15 -0600

    Cleaned main window

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,6 @@
-import { saveCredential, saveSecret } from './services/storage'
-
 export function App() {
   return (
     <main>
-      <h1 className="w-full text-center text-2xl font-semibold">ezVault</h1>
-
-      <button
-        onClick={() => {
-          saveCredential({
-            username: 'john.doe',
-            password: 'password',
-            app_id: '1234567890',
-            created_at_ts: 1234567890,
-            updated_at_ts: 1234567890,
-          })
-        }}
-      >
-        Save Credential
-      </button>
-
-      <button
-        onClick={() => {
-          saveSecret({
-            key: 'API_KEY',
-            value: '1234567890',
-            description: 'This is a note',
-            app_id: '1234567890',
-            created_at_ts: 1234567890,
-            updated_at_ts: 1234567890,
-          })
-        }}
-      >
-        Save Secret
-      </button>
     </main>
   )
 }

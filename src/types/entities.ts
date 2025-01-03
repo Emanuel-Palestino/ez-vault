b6df7be80c40080c53cd4441fb6cf732c5029b9e
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2025-01-02 19:08:39 -0600

    Minor format change

--- src/types/entities.ts
+++ src/types/entities.ts
@@ -8,12 +8,14 @@ export type NewEnvironment = Omit<
   Environment,
   'id' | 'created_at_ts' | 'updated_at_ts'
 >
+
 export type Environment = BaseType & {
   id: string
   name: string
 }
 
 export type NewApp = Omit<App, 'id' | 'created_at_ts' | 'updated_at_ts'>
+
 export type App = BaseType & {
   id: string
   url: string // js URL type
@@ -27,6 +29,7 @@ export type NewPort = Omit<
   Port,
   'id' | 'created_at_ts' | 'updated_at_ts' | 'app'
 > & { app_id: string }
+
 export type Port = BaseType & {
   id: string
   app: App
@@ -37,6 +40,7 @@ export type NewCredential = Omit<
   Credential,
   'id' | 'created_at_ts' | 'updated_at_ts' | 'app'
 > & { app_id: string }
+
 export type Credential = BaseType & {
   id: string
   app: App
@@ -48,6 +52,7 @@ export type NewSecret = Omit<
   Secret,
   'id' | 'created_at_ts' | 'updated_at_ts' | 'app'
 > & { app_id: string }
+
 export type Secret = BaseType & {
   id: string
   app: App
